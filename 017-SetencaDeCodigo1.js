e9482936be46bb1e637276690392b60c83662e01
Telmo Fernandes <telmorf@live.com>
2024-03-01 20:30:13 +0000

    Initial Commit

---
+++ 017-SetencaDeCodigo1.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo no ecrã !
+console.log("Bom Dia!");
+console.log("Boa Tarde!");
+console.log("Boa Noite!");
+
+/*
+console.log("Bom Dia!");
+console.log("Boa Tarde!");
+console.log("Boa Noite!");
+*/
