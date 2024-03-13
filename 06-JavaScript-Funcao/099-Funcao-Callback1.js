f364837a7fa9cbce964fa5e3ebcbe87df985f7e8
Telmo Fernandes <telmorf@live.com>
2024-03-13 10:20:03 +0000

    05JavaScript-Funcoes-Aula105

--- 06-JavaScript-Funcao/099-Funcao-Callback1.js
+++ 06-JavaScript-Funcao/099-Funcao-Callback1.js
@@ -4,5 +4,6 @@ function imprimir(nome, indice) {
     console.log(`${indice + 1}. ${nome}`)
 }
 
-fabricantes.forEach(imprimir)
+fabricantes.forEach(imprimir)       // Passa o elemento e o indice no array
+console.log()
 fabricantes.forEach(fabricante => console.log(fabricante))
--- 06-JavaScript-Funcao/100-Funcao-Callback2.js
+++ 06-JavaScript-Funcao/100-Funcao-Callback2.js
@@ -7,15 +7,16 @@ for (let i in notas) {
         notasBaixas1.push(notas[i])
     }
 }
-
 console.log(notasBaixas1)
 
 // Com callback
 const notasBaixas2 = notas.filter(function (nota) {
     return nota < 7
 })
-
 console.log(notasBaixas2)
+
+const notasBaixas4 = notas.filter(nota => nota < 7 )
+console.log(notasBaixas4)
 
 const notasMenorQue7 = nota => nota < 7
 const notasBaixas3 = notas.filter(notasMenorQue7)
